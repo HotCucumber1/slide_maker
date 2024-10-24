5890705eac95f021b9d4c9d71bc1da5976a61af5
Dmitriy <shadowpersik05@mail.ru>
2024-10-24 23:12:10 +0300

    master Добавил правильную логику удаления слайда

--- src/store/deleteSlide.ts
+++ src/store/deleteSlide.ts
@@ -2,13 +2,36 @@ import {Editor} from "./editor.ts";
 
 function deleteSlides(editor: Editor): Editor
 {
+    const lastSelectedSlideId = editor.selectedSlides[editor.selectedSlides.length - 1];
+    let newActiveSlidePos = editor.presentation.slides
+                                                    .map((slide) => slide.id)
+                                                    .indexOf(lastSelectedSlideId) + 1;
+
+    if (newActiveSlidePos > editor.presentation.slides.length - 1)
+    {
+        const firstSelectedSlideId = editor.selectedSlides[editor.selectedSlides.length - 1];
+        newActiveSlidePos = editor.presentation.slides
+                                            .map((slide) => slide.id)
+                                            .indexOf(firstSelectedSlideId) - 1;
+    }
+
+    let selection = [];
+    let active = "";
+    if (newActiveSlidePos >= 0)
+    {
+        const newActiveSlideId =  editor.presentation.slides[newActiveSlidePos].id;
+        selection = [newActiveSlideId];
+        active = newActiveSlideId;
+    }
+
     const newSlides = editor.presentation.slides.filter(
         slide => editor.selectedSlides.indexOf(slide.id) === -1
     );
-    editor.selectedSlides = [];
 
     return  {
         ...editor,
+        selectedSlides: selection,
+        currentSlideId: active,
         presentation: {
             ...editor.presentation,
             slides: newSlides,
